550da40f487fa583c73af801d71fa8e30a7273f7
Igor Tarasiński <igoss1037@gmail.com>
2023-10-15 19:29:56 +0200

    Adding Area

---
+++ Cinema.Mvc/Areas/Admin/Controllers/MovieController.cs
@@ -0,0 +1,94 @@
+﻿using Cinema.Application.Movie.Queries.GetAllMovies;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cinema.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MovieController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public MovieController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET: MovieController
+        public async Task<ActionResult> Index()
+        {
+            var movies = await _mediator.Send(new GetAllMoviesQuery());
+
+            return View(movies);
+        }
+
+        // GET: MovieController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MovieController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MovieController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MovieController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MovieController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MovieController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MovieController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Cinema.Mvc/Program.cs
+++ Cinema.Mvc/Program.cs
@@ -26,10 +26,14 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapRazorPages();
+
+app.MapControllerRoute(
+  name: "areas",
+  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapRazorPages();
 
 app.Run();
