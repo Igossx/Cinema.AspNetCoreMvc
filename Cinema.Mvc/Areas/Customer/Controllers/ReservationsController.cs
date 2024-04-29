b757d2d916fc19388d7f07796173fd1412e70b71
Igor Tarasiński <igoss1037@gmail.com>
2024-04-29 20:58:01 +0200

    Adding delete_modal and filtering reservations

--- Cinema.Mvc/Areas/Customer/Controllers/ReservationsController.cs
+++ Cinema.Mvc/Areas/Customer/Controllers/ReservationsController.cs
@@ -1,4 +1,6 @@
-﻿using Cinema.Application.Reservation.Queries.GetAllReservationsUser;
+﻿using Cinema.Application.Reservation.Commands.DeleteReservation;
+using Cinema.Application.Reservation.Queries.GetAllReservationsUser;
+using Cinema.Mvc.Extensions;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -16,11 +18,35 @@ namespace Cinema.Mvc.Areas.Customer.Controllers
             _mediator = mediator;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string? sortOrder)
         {
+            ViewBag.SortOrder = string.IsNullOrEmpty(sortOrder) ? "asc" : sortOrder;
+
             var reservations = await _mediator.Send(new GetAllReservationsUserQuery());
 
+            switch (sortOrder)
+            {
+                case "desc":
+                    reservations = reservations.OrderByDescending(r => r.TotalCost);
+                    break;
+                default:
+                    reservations = reservations.OrderBy(r => r.TotalCost);
+                    break;
+            }
+
             return View(reservations);
+        }
+
+        // POST: Reservation/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            await _mediator.Send(new DeleteReservationCommand() { Id = id });
+
+            this.SetNotificaton("success", "Reservation deleted.");
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
